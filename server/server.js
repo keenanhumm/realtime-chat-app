d0117bb0e4325b020bf5cc891cc80930e31cb965
Keenan Humm <keenan.humm@gmail.com>
2021-07-26 15:52:07 -0500

    implements get messages and post message, adds client dir

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+const messages = [];
+
 const typeDefs = `
   type Message {
     id: ID!
@@ -8,20 +10,36 @@ const typeDefs = `
   }
 
   type Query {
-    messages: [Messages!]
+    messages: [Message!]
+  }
+
+  type Mutation {
+    postMessage(user: String!, content: String!): ID! 
   }
 `;
 
 const resolvers = {
   Query: {
-    messages: () => [],
+    messages: () => messages,
+  },
+  Mutation: {
+    postMessage: (_, { user, content }) => {
+      const id = messages.length;
+      messages.push({
+        id,
+        user,
+        content,
+      });
+      return id;
+    },
   },
 };
 
 const server = new GraphQLServer({
+  resolvers,
   typeDefs,
 });
 
 server.start(({ port }) => {
-  console.log(`server started on port ${port}`);
+  console.log(`server running @ http://localhost:${port}`);
 });
